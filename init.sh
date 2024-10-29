70444414560974e4750d5d2347194cf08106d3ce
dyhkwong <50692134+dyhkwong@users.noreply.github.com>
2024-10-30 01:12:46 +0800

    Update core

--- init.sh
+++ init.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 go get -v -d
-go install -v golang.org/x/mobile/cmd/gomobile@v0.0.0-20241004191011-08a83c5af9f8
-go install -v golang.org/x/mobile/cmd/gobind@v0.0.0-20241004191011-08a83c5af9f8
+go install -v golang.org/x/mobile/cmd/gomobile@v0.0.0-20241016134751-7ff83004ec2c
+go install -v golang.org/x/mobile/cmd/gobind@v0.0.0-20241016134751-7ff83004ec2c
 gomobile init
